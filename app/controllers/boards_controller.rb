c8cd8419bd8af83a0a174fdb95924c6289667f4b
Iryna Stein <isophiestein@gmail.com>
2021-09-21 20:24:38 -0600

    quote_id

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,13 +1,13 @@
 class BoardsController < ApplicationController
+
   def create
     user = User.find_by(id: session[:user_id])
+    stickers = Sticker.where("category=?", params[:category])
     board = user.boards.new(board_params)
-    byebug
     board.quote = Quote.where("category=?", params[:category]).first
     board.name = 'Untitled-board-' + Date.today.to_s + rand(100..999).to_s if params[:name].nil?
     board.save if board.valid?
-    byebug
-    render json: board, status: 200
+    render json: {board: board, assets: {stickers: stickers}}, status: 200
   end
 
   private
