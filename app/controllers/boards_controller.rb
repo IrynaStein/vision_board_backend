d8bac22df6439597903a7570e81a2d346e5ff9ef
Iryna Stein <isophiestein@gmail.com>
2021-09-25 17:28:29 -0600

    handled creation of only one board in category

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,16 +1,19 @@
 class BoardsController < ApplicationController
-
   def create
     user = User.find_by(id: session[:user_id])
-    stickers = Sticker.where("category=?", board_params[:category])
-    board = user.boards.build(board_params)
-    board.set_name(board_params)
-    # board.set_quote(board_params)
-    board.save!
-    render json: {board: board, assets: {stickers: stickers}}, status: 200
+    if user.boards.where('category=?', board_params[:category]).exists?
+       render json: { errors: 'Seems like you already have a board in this category. Please chose a different category or delete existing board' }, status: :unprocessable_entity 
+    else
+      stickers = Sticker.where('category=?', board_params[:category])
+      board = user.boards.build(board_params)
+      board.set_name(board_params)
+      # board.set_quote(board_params)
+      board.save!
+      render json: { board: board, assets: { stickers: stickers } }, status: 200
+    end
   end
 
-  def update 
+  def update
     board = Board.find_by(id: params[:id])
     board.update(board_params)
     render json: board, status: 200
